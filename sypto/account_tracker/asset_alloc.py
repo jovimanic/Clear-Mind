af189a38653e2647afe06b5e435699fac734fe0d
Prakash C <prakashchidambaram2001@gmail.com>
2022-05-13 14:08:24 +0530

    initial asset_allocation

---
+++ sypto/account_tracker/asset_alloc.py
@@ -0,0 +1,47 @@
+import ccxt
+
+def asset_allocation():
+	binance = ccxt.binance()
+	exchange_id = 'binance'
+	exchange_class = getattr(ccxt, exchange_id)
+	exchange = exchange_class({
+		'apiKey' : '2Zuss4JKwsrMrq4DoG7Y8avsj9CA538S1NSd2sgT3JUE4LXO8gwKOV7IPUNjRb6z',
+		'secret' : 'G4Eo2iGX1Nh1vIDMNRnAFEuBXFsHQem7Kq7XtRvU5G3BeKXSBTwmqeV2aSiL3Jff'
+		})
+	
+	available = exchange.fetch_balance()
+	total = available['total']
+	quantity = {}
+	for i in total:
+		if total[i] != 0.0:
+			quantity[i] = total[i]
+		
+	price = {}
+	for i in quantity:
+		val = i+'/USDT'
+		if i == 'USDT':
+			price[i] = 1.0
+			continue
+		price[i] = binance.fetch_ticker(val)['close']
+
+	total_holding = {}	
+	for i in quantity:
+		total_holding[i] = quantity[i]*price[i]
+	
+	total_USDT = 0.0
+	for i in total_holding:
+		total_USDT += total_holding[i]
+	
+	for i in total_holding:
+		total_holding[i] = total_holding[i]/total_USDT*100
+	
+	print(total_holding)
+	
+if __name__ == '__main__':
+	asset_allocation()
+
+	
+	
+	
+	
+		
